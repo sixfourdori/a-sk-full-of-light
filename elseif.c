a7cd417f5c09dd7bd9f902dc80f4945659c9cf76
sixfourdori <129130041+sixfourdori@users.noreply.github.com>
2023-04-18 01:20:17 +0000

    그동안 배웠던 else if로 좋아하는 음식 조사해보기~함~

---
+++ elseif.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main() {
+  printf("1번 사과, 2번 수박, 3번 생크림케이크, 4번 잔치국수\n");
+  int x;
+  scanf("%d", &x);
+  if(x==1)
+    printf("사과를 좋아하군요!");
+  else if (x==2)
+    printf("응~ 난 안좋아해~");
+  else if(x==3)
+    printf("나도~ㅎㅎ");
+  else if(x==4)
+    printf("저도 잔치국수 국물을 참 좋아해요!");
+  else 
+    printf("당신은 아마도 맛난걸 좋아하지 않는 것 같군요!");
+  printf("참 불행해보여요~ 좋아하는 음식 조사 끝!");
+}
