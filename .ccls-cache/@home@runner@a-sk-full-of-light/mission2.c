3866c0f9b4f234cc415a448e7d49bd134aba6a9d
sixfourdori <129130041+sixfourdori@users.noreply.github.com>
2023-05-04 01:24:49 +0000

    else if 조건으로 시험점수매겨보았다~
     아직 혼자 조건문 완벽히 만들기는 좀 무린듯~ㅎ

---
+++ .ccls-cache/@home@runner@a-sk-full-of-light/mission2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+main(){
+ int a;
+ printf("점수를 입력하세요");
+ scanf("%d", &a);
+
+ if(a>=90)
+   printf("A학점");
+ else if(a>=80)
+   printf("B학점");
+ else if(a>=70)
+   printf("C학점");
+ else if(a>=60)
+   printf("D학점");
+ else if(60>a)
+   printf("E학점");
+ else 
+   printf("당신이 너무 잘해서 여기서는 담을 수가 없네요~");
+}
