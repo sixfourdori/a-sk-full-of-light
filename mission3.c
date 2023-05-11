6faec97a97bafd73bf797fd634bc73cc24edfbe7
sixfourdori <129130041+sixfourdori@users.noreply.github.com>
2023-05-11 01:21:21 +0000

    if조건문 대신 switch로 과일문(?) 배워보았다~ switch에서는 break필수~~!

---
+++ mission3.c
@@ -0,0 +1,13 @@
+// #include<stdio.h>
+// main(){
+//   int a; 
+//   printf("정수를 입력: ");
+//     scanf("%d",&a);
+//   int b;
+//  printf("정수를 입력: ");
+//   scanf("%d",&b);
+//   if(a>=b)
+//     printf("%d",a);
+//   else if(a<=b)
+//     printf("%d",b);
+// }
